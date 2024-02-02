43ed82e8e4fecc4579d040271843fdfd5a59f270
vikzh <vikt.anmail@gmail.com>
2024-02-02 02:59:08 +0100

    save images added

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 import axios from 'axios';
 import fs from 'node:fs/promises';
-
+import * as cheerio from 'cheerio';
 const getPageData = (url) =>
   axios.get(url)
     .then((response) =>
       response.data);
 
-const writeDataToFile = (data) => {
-  fs.writeFile('index.html', data);
+const writeDataToFile = (data, name = 'index.html') => {
+  fs.writeFile(`./_files/${name}`, data);
 }
 
+const saveSource = (url, fileName, dir) => {
+  axios.get(url, {responseType: "arraybuffer"})
+    .then(response => {
+      return writeDataToFile(response.data, fileName);
+    });
+}
+
+const saveImages = (data) => {
+  const $ = cheerio.load(data);
+  const imgTags = $('img');
+  for(const imgTag of imgTags) {
+    const regexp = /([^\/]+$)/g;
+    const [[fileName]] = [...imgTag.attribs.src.matchAll(regexp)];
+
+    saveSource(imgTag.attribs.src, fileName)
+    imgTag.attribs.src = `./_files/${fileName}`;
+  }
+
+  console.log(imgTags);
+  return data;
+}
 const savePage = (url) =>
   getPageData(url)
+    .then(data => {
+      fs.mkdir('./_files', {recursive: true});
+
+      return data;
+    })
+    .then(saveImages)
     .then(writeDataToFile)
 
 export default savePage;
